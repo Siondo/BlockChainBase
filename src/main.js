a3e3d0306f74c90dc565211b4109b3407715d3b0
辛巴 <email>
2022-08-11 01:31:07 +0800

    mac测试

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import 'element-ui/lib/theme-chalk/index.css';
 import "./utils/register.js";
 import 'lib-flexible/flexible.js'
 import * as echarts from 'echarts'
-// 导入ICon 资源
+// 导入阿里巴巴ICon 资源
 import './assets/icon/iconfont.css';
 
 Vue.prototype.$echarts = echarts
